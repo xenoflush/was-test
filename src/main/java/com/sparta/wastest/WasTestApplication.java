6456e4b85cc6246c7fb09ffec28106914e83b2ae
jh.kim <start4321@hanmail.net>
2024-04-16 16:38:32 +0900

    feat: init commit

---
+++ src/main/java/com/sparta/wastest/WasTestApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.wastest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WasTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/wastest/WasTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.wastest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
